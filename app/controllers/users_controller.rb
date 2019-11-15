a931afa498c2b98d88a9bda17bfc5cc9f593d2ce
seregaskirev <seregaskirev@gmail.com>
2019-11-15 11:14:31 +0300

    users api changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,45 +4,39 @@ class UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: { data: users }
+    render json: { items: users, count: users.count }
   end
 
   def show
-    render json: { data: @user }
+    render json: @user
   end
 
   def current
-    render json: { data: current_user }
+    render json: current_user
   end
 
   def create
-    authorize! :create, User
-
     user = User.new user_params
 
     if user.save
-      render json: { data: user }
+      render json: user
     else
       error! errors: user.errors
     end
   end
 
   def update
-    authorize! :update, @user
-
     if @user.update(user_params)
-      render json: { data: @user }
+      render json: @user
     else
       error! errors: @user.errors
     end
   end
 
   def update_password
-    authorize! :update, @user
-
     return error!({ messages: ['Please, enter correct current password'] }, 403) unless @user.authenticate(old_password)
 
-    if @user.update(password_params)
+    if @user.update(password: new_password)
       head 200
     else
       error! errors: @user.errors
@@ -50,8 +44,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    authorize! :delete, @user
-
     @user.destroy
   end
 
@@ -65,11 +57,11 @@ class UsersController < ApplicationController
     params.permit(:email, :first_name, :last_name, :password)
   end
 
-  def password_params
-    params.permit(:password)
-  end
-
   def old_password
     params[:old_password]
+  end
+
+  def new_password
+    params[:new_password]
   end
 end
